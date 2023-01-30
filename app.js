8d52fa86c85c45c39b523915c6d068ffbf6c693b
Alohaiscoding <andreagcumunel@gmail.com>
2023-01-30 20:39:04 +1100

    I added two buttons html

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 function displayTemperature(response) {
   console.log(response.data);
 }
+
 let apiKey = "508d54a034ao7702ct57a2eb3f9ba0acb";
 let apiUrl =
   "https://api.shecodes.io/weather/v1/current?query=Sydney&key=08d54a034ao7702ct57a2eb3f9ba0acb&units=metric";
+console.log(apiUrl);
 
 axios.get(apiUrl).then(displayTemperature);
