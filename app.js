daf25740657612cf81ddd2838ee4717e34b4dd52
Alohaiscoding <andreagcumunel@gmail.com>
2023-01-31 11:14:40 +1100

    I added js humidity, wind, description + real time date

--- app.js
+++ app.js
@@ -1,5 +1,49 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayTemperature(response) {
-  console.log(response.data.temperature);
+  console.log(response.data);
+  let temperature = document.querySelector("#temperature");
+  temperature.innerHTML = Math.round(response.data.temperature.current);
+  let city = document.querySelector("#city");
+  city.innerHTML = response.data.city;
+  let description = document.querySelector("#description");
+  description.innerHTML = response.data.condition.description;
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = response.data.temperature.humidity;
+  let wind = document.querySelector("#wind");
+  wind.innerHTML = Math.round(response.data.wind.speed);
+  let date = document.querySelector("#date");
+  date.innerHTML = formatDate(response.data.time * 1000);
 }
 
 let apiKey = "508d54a034ao7702ct57a2eb3f9ba0acb";
